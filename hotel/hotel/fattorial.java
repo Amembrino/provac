9bc23c2bd22d6f6d9ffdc4da8368b53c005384f4
Amembrino <34981889+Amembrino@users.noreply.github.com>
2018-01-29 19:00:26 +0100

    Update fattorial.java

--- hotel/hotel/fattorial.java
+++ hotel/hotel/fattorial.java
@@ -5,13 +5,13 @@ public class fattorial {
 	/* Calcola il fattoriale di n. */ 
 	public static int fattoriale(int n) {
 	// pre: n>=0 
-	int fattttt;   // il fattoriale di n
+	int fat;   // il fattoriale di n
 	int i;      // per iterare tra 1 e n 
 	/* calcola il fattoriale di n */ 
 	fatt = 1; 
 	for (i=1; i<n; i++) {
 	fatt *= i; 
 	}
-	return fattttt; 
+	return fat; 
 	}
 }
