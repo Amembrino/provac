bd7fe90a42819204b21de5b48d35eb22a6d5f6cd
Ant0ni0MEM <32262819+Ant0ni0MEM@users.noreply.github.com>
2018-01-29 19:02:09 +0100

    mer

--- hotel/hotel/fattorial.java
+++ hotel/hotel/fattorial.java
@@ -5,13 +5,13 @@ public class fattorial {
 	/* Calcola il fattoriale di n. */ 
 	public static int fattoriale(int n) {
 	// pre: n>=0 
-	int fat;   // il fattoriale di n
+	int fattor;   // il fattoriale di n
 	int i;      // per iterare tra 1 e n 
 	/* calcola il fattoriale di n */ 
 	fatt = 1; 
 	for (i=1; i<n; i++) {
 	fatt *= i; 
 	}
-	return fat; 
+	return fattor; 
 	}
 }
